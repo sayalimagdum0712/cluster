4fd22d988e694645a102e5288050f7c2b2402a83
Sayali Magdum07 <71891482+sayalimagdum0712@users.noreply.github.com>
2026-06-11 13:24:04 +0530

    Update and rename README.md to README.tf

---
+++ README.tf
@@ -0,0 +1,44 @@
+# terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "jenkins-eks"
+  kubernetes_version = "1.33"
+
+  vpc_id = "YOUR_VPC_ID"
+
+  subnet_ids = [
+    "YOUR_SUBNET_1",
+    "YOUR_SUBNET_2"
+  ]
+
+  eks_managed_node_groups = {
+    workers = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+    Terraform   = "true"
+  }
+}
